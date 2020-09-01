62ecca855d70c8bd3df4b9aecdd55f8d3b26228e
Ria Pacheco <53801436+riapacheco@users.noreply.github.com>
2020-08-31 22:03:20 -0600

    Setup

---
+++ scripts/myscripts.js
@@ -0,0 +1,28 @@
+
+/* -------------------------------------------------------------------------- */
+/*                            Navigation Functions                            */
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------- Mobile Menu Click Events ----------------------- */
+
+// show menu
+function showMobileNav() {
+  let x = document.getElementById("mobileNavMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// replace hamburger icon with close icon only when menu is showing
+function removeHam() {
+  let y = document.getElementById("mobileNavMenu");
+  let z = document.getElementById("mobileIcon");
+
+  if (y.style.display === "block") {
+    z.classList = 'ion-md-close mobile-menu-icon';
+  } else {
+    z.classList = 'ion-ios-menu mobile-menu-icon';
+  }
+}
